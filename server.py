3661e01e4deedc3b2b3e31dce8e32a53e3f3f61e
Wayne <tohwsw@gmail.com>
2022-02-24 11:31:11 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,61 @@
-from flask import Flask
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
+import boto3
+import mysql.connector
+
 import os
 
-PORT = 8080
-name = os.environ['NAME']
-if name == None or len(name) == 0:
-  name = "world"
-MESSAGE = "Hello, " + name + "!"
-print("Message: '" + MESSAGE + "'")
+DATABASE_REGION = 'eu-west-1'
+DATABASE_CERT = 'cert/eu-west-1-bundle.pem'
+DATABASE_HOST = os.environ['DATABASE_HOST']
+DATABASE_PORT = os.environ['DATABASE_PORT']
+DATABASE_USER = os.environ['DATABASE_USER']
+DATABASE_NAME = os.environ['DATABASE_NAME']
 
-app = Flask(__name__)
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
 
+PORT = int(os.environ.get('PORT'))
 
-@app.route("/")
-def root():
-  print("Handling web request. Returning message.")
-  result = MESSAGE.encode("utf-8")
-  return result
+rds = boto3.client('rds')
 
+try:
+    token = rds.generate_db_auth_token(
+        DBHostname=DATABASE_HOST,
+        Port=DATABASE_PORT,
+        DBUsername=DATABASE_USER,
+        Region=DATABASE_REGION
+    )
+    mydb =  mysql.connector.connect(
+        host=DATABASE_HOST,
+        user=DATABASE_USER,
+        passwd=token,
+        port=DATABASE_PORT,
+        database=DATABASE_NAME,
+        ssl_ca=DATABASE_CERT
+    )
+except Exception as e:
+    print('Database connection failed due to {}'.format(e))          
 
-if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=PORT)
+def all_books(request):
+    mycursor = mydb.cursor()
+    mycursor.execute('SELECT name, title, year FROM authors, books WHERE authors.authorId = books.authorId ORDER BY year')
+    title = 'Books'
+    message = '<html><head><title>' + title + '</title></head><body>'
+    message += '<h1>' + title + '</h1>'
+    message += '<ul>'
+    for (name, title, year) in mycursor:
+        message += '<li>' + name + ' - ' + title + ' (' + str(year) + ')</li>'
+    message += '</ul>'
+    message += '</body></html>'
+    return Response(message)
+
+if __name__ == '__main__':
+
+    with Configurator() as config:
+        config.add_route('all_books', '/')
+        config.add_view(all_books, route_name='all_books')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', PORT, app)
+    server.serve_forever()
